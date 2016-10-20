aca81ed1152f621e355b3625408203e94dc2ccca
liutao <449711315@qq.com>
2016-10-20 22:20:05 +0800

    first commit

--- app/src/main/java/com/example/liutao/androidgitthrees/MainActivity.java
+++ app/src/main/java/com/example/liutao/androidgitthrees/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.liutao.androidgitthrees;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//一号
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //我是二号
+        //我是一号
+        //我是一一号
         setContentView(R.layout.activity_main);
     }
 }
